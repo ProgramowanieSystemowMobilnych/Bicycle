94badd368856278ee66b673b49edd6fa7b0861a7
Kamil Ślósarczyk <48627674+BlackSlusarz@users.noreply.github.com>
2020-04-04 14:48:53 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,74 +1,32 @@
-const cacheName = 'v1';
-const cacheFiles = [
-    './',
-    './index.html',
-    './favicons/icon-72x72.png',
-    './favicons/icon-96x96.png',
-    './favicons/icon-128x128.png',
-    './favicons/icon-144x144.png',
-    './favicons/icon-152x152.png',
-    './favicons/icon-192x192.png',
-    './favicons/icon-384x384.png',
-    './favicons/icon-512x512.png',
-    './css/styles.css',
-    './js/main.js'
-];
 
-
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Installed');
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log('[ServiceWorker] Caching cacheFiles');
-            return cache.addAll(cacheFiles);
-        })
+self.addEventListener('install', function (event) {
+    console.log('SW Installed');
+    event.waitUntil(
+        caches.open('static')
+            .then(function (cache) {
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    './css/styles.css',
+                    './js/main.js',
+                ]);
+            })
     );
 });
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activated');
-        e.waitUntil(
 
-            caches.keys().then(function(cacheNames) {
-                return Promise.all(cacheNames.map(function(thisCacheName) {
-                    if (thisCacheName !== cacheName) {
-                        console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
-                        return caches.delete(thisCacheName);
-                }
-            }));
-        })
-    );
-
+self.addEventListener('activate', function () {
+    console.log('SW Activated');
 });
-self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
-    e.respondWith(
-        caches.match(e.request)
-            .then(function(response) {
-                if ( response ) {
-                    console.log("[ServiceWorker] Found in Cache", e.request.url, response);
-                    return response;
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(res) {
+                if (res) {
+                    return res;
+                } else {
+                    return fetch(event.request);
                 }
-                const requestClone = e.request.clone();
-                return fetch(requestClone)
-                    .then(function(response) {
-
-                        if ( !response ) {
-                            console.log("[ServiceWorker] No response from fetch ");
-                            return response;
-                        }
-
-                        const responseClone = response.clone();
-                        caches.open(cacheName).then(function(cache) {
-                            cache.put(e.request, responseClone);
-                            console.log('[ServiceWorker] New Data Cached', e.request.url);
-                            return response;
-
-                        });
-
-                    })
-                    .catch(function(err) {
-                        console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
-                    });
             })
     );
 });
