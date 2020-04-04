e45c6769d7a3eb027d4cafc9d03eaa553d39f0d4
Kamil Ślósarczyk <48627674+BlackSlusarz@users.noreply.github.com>
2020-04-04 14:49:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-
 if ('serviceWorker' in navigator) {
-
-    navigator.serviceWorker
-        .register('./serviceWorker.js', { scope: './' })
-        .then(function(registration) {
-            console.log("Service Worker Registered. Scope is:"+registration.scope);
-        })
-        .catch(function(err) {
-            console.log("Service Worker Failed to Register", err);
-        })
-
+    navigator.serviceWorker.register('/serviceWorker.js')
+        .then(function() {
+            console.log('SW registered');
+        });
 }
-
